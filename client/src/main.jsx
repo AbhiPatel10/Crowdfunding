2dcb78372eabd45b300754762bbaa18461eafb0e
Abhi Patel <mr.abhiofficial100@gmail.com>
2023-03-27 13:20:46 +0530

    Project setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {ChainId, ThirdwebAuthProvider, ThirdwebProvider} from '@thirdweb-dev/react';
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebAuthProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App /> 
-        </Router>
-    </ThirdwebAuthProvider>
-)
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+      <Router>
+        <App />
+      </Router>
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
