4b6963e5b95acfad0f86c3441e35119f0188a333
Abhi Patel <mr.abhiofficial100@gmail.com>
2023-04-18 00:28:11 +0530

    Crowdfunding Project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { Sepolia } from "@thirdweb-dev/chains";
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-      <Router>
+  <ThirdwebProvider activeChain={ Sepolia }> 
+    <Router>
+      <StateContextProvider>
         <App />
-      </Router>
-    </ThirdwebProvider>
-  </React.StrictMode>
-);
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
+)
